db542b932ec94dc493f6ec03604c29534fb7064c
Richard Sithole <richardsit@gmail.com>
2021-09-24 16:06:46 +0200

    DODO-14679 clean up code comments

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import * as lib from "./library.js";
 
-// Listen to "message" type event.
-// Ref: https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage#the_dispatched_event
+// Listen to "message" type events.
 window.addEventListener("message", handlePostMessage, false);
 
 let webclientOrigin;
@@ -9,70 +8,65 @@ let lastSelectedEntryOsid;
 let currentSelectedOSIDs = [];
 let dashletName = "Dashlet";
 
-// A function responsible for processing all incoming "messages" from enaio® webclient.
-function handlePostMessage(e) {
-  webclientOrigin = e.origin;
-
-  // Note, in enaio desktop client, srcOrigin is represented as "file://"
-  const safeOrigin = ["file://", "http://10.6.0.197", "http://10.6.0.165"].includes(webclientOrigin);
-  if (!safeOrigin) {
-    console.log(`webclientOrigin ${webclientOrigin} is different from srcOrigin ${srcOrigin}`);
-    return false;
-  }
+/**
+ * A function responsible for processing all incoming "messages" from the enaio® webclient.
+ * @param event the object passed from the other Window i.e. enaio® webclient.
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage#the_dispatched_event
+ */
+function handlePostMessage(event) {
+  webclientOrigin = event.origin;
 
   /* Ensure "messages" come from a trusted source i.e. your own enaio® hosted domain.
      Ref: https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage#security_concerns
 
      "srcOrigin" is the domain URL where enaio® webclient is served. Example: https://enaio.company-name.de
+      Please note, in enaio desktop client, "srcOrigin" is represented as "file://" string.
 
-    //  Uncomment the below code and replace "srcOrigin" variable with your own enaio® webclient domain.
-    const srcOrigin = "http://your-enaio-domain" || "https://your-enaio-domain";
-    if (webclientOrigin !== srcOrigin) {
-      return false;
-    }
+     Uncomment the below code and replace "srcOrigin" variable with your own enaio® webclient domain.
+      
+     // const safeOrigin = ["file://", "https://your-enaio-domain"].includes(webclientOrigin);
+     // if (!safeOrigin) {
+     //   console.log(`webclientOrigin ${webclientOrigin} is different from srcOrigin ${srcOrigin}`);
+     //   return false;
+     // }
   */
 
   // "handleWebclientMessage" is a handler function which further processes all incoming "messages" from enaio® webclient (see implimentation details in the library.js file).
   // Extract the "type" and "data" properties for further processing.
   // Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-  const { type, data } = lib.handleWebclientMessage(e.data);
-  let context = "";
+  const { type, data } = lib.handleWebclientMessage(event.data);
 
   if (type === "onInit") {
+    // Do initialization work here.
     initDashlet(data);
     console.log(`onInit event data:`, data);
-    // "context" will be made available (in the onInit type event) in a later enaio® webclient service release. (TODO)
   } else if (type === "onUpdate") {
+    // React to osid selection changes here.
     initDashlet(data);
-    context = data.context;
-    console.log(`Current context:`, context);
     console.log(`onUpdate event data:`, data);
   }
+
   return true;
 }
 
-//
 /**
- * Initialize the dashlet by it feeding incoming enaio® webclient data.
+ * Initialize the dashlet by feeding it incoming enaio® webclient data. The method has no return value.
  * @param data an object which contains enaio® webclient properties that the dashlet can use to enrich itself.
- * @returns The method has no return value.
  */
 function initDashlet(data) {
   dashletName =
     data.activeCustomDashlet[
-    "title_" + data.sessionInfo.language.toUpperCase()
+      "title_" + data.sessionInfo.language.toUpperCase()
     ] || "Dashlet";
   lastSelectedEntryOsid = data.lastSelectedEntry.osid;
   currentSelectedOSIDs = data.selectedEntries.map((dmsInfo) => dmsInfo.osid);
-  // Uncomment the below code to see an array of the hitlist's currently osids.
+  // Uncomment the below code to see an array of the hitlist's currently selected osid(s).
   // console.log(`Currently selected osids`, currentSelectedOSIDs);
 }
 
 /**
  * Opens the location in the current browser tab (or a location selection in the case of several possible locations) for the DMS object transferred as a parameter.
- * @param null no arguments are required for this method.
- * @returns The method has no return value. You should provide them with a try-catch-block or error handler, since an error is triggered in the event of an error.
- * This would be caught in the catch block or error handler.
+ * The method has no return value.
  * @link https://help.optimal-systems.com/enaio_develop/display/WEB/openLocation
  */
 async function openLocation() {
@@ -88,9 +82,7 @@ async function openLocation() {
 
 /**
  * Opens the location in a new browser tab (or a location selection in the case of several possible locations) for the DMS object transferred as a parameter.
- * @param null no arguments are required for this method.
- * @returns The method has no return value. You should provide them with a try-catch-block or error handler, since an error is triggered in the event of an error.
- * This would be caught in the catch block or error handler.
+ * The method has no return value.
  * @link https://help.optimal-systems.com/enaio_develop/display/WEB/openLocation
  */
 async function openLocationNewTab() {
@@ -105,9 +97,8 @@ async function openLocationNewTab() {
 }
 
 /**
- * Open indexdata mask for the currently selected osid.
- * @param null no arguments are required for this method.
- * @returns Boolean true if the objectId and objectTypeId were valid and the opening was successful. Otherwise false.
+ * Opens the indexdata mask for the currently selected osid.
+ * @returns Boolean true if the objectId and objectTypeId are valid and the opening was successful. Otherwise false.
  * @link https://help.optimal-systems.com/enaio_develop/display/WEB/openIndexData
  */
 async function openIndexData() {
@@ -123,8 +114,7 @@ async function openIndexData() {
 
 /**
  * Query the currently selected objects.
- * @param null no arguments are required for this method.
- * @returns Depending on whether you call getSelectedObjects or the enaio® RichClient compatibility method,
+ * Depending on whether you call getSelectedObjects or the enaio® RichClient compatibility method,
  * you will get a different result. In the former, a JavaScript array with objects consisting of objectId and
  * objectTypeId of the selected DMS objects. With the compatibility method, a character sect that is separated
  * by a semicolon and returns a tuple from objectId and objectTypeId.
