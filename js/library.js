c4ed353a082f7b497ad60a8af5c213cdf5a7e962
Richard Sithole <richardsit@gmail.com>
2021-09-24 16:43:59 +0200

    DODO-14465 replace alert() with inline html

--- js/library.js
+++ js/library.js
@@ -8,27 +8,24 @@ const alertQueue = [];
  * @link https://help.optimal-systems.com/enaio_develop/display/WEB/5.2+Kommunikation
  */
 function handleWebclientMessage(payload) {
-  if (payload.msgId && msgQueue[payload.msgId]) {
-    if (payload.data.result !== undefined) {
-      msgQueue[payload.msgId].resolve(payload.data.result);
-    } else if (payload.data.error !== undefined) {
-      msgQueue[payload.msgId].reject(payload.data.error);
+    if (payload.msgId && msgQueue[payload.msgId]) {
+        if (payload.data.result !== undefined) {
+            msgQueue[payload.msgId].resolve(payload.data.result);
+        } else if (payload.data.error !== undefined) {
+            msgQueue[payload.msgId].reject(payload.data.error);
+        }
+
+        if (alertQueue.includes(payload.msgId)) {
+            // display payload info
+            const selectedObjectsContainer = document.getElementById("selectedObjectsContainer");
+            selectedObjectsContainer.innerHTML = `${JSON.stringify(payload.data.result)}`;
+            alertQueue.splice(alertQueue.indexOf(payload.msgId), 1);
+        }
+
+        delete msgQueue[payload.msgId];
     }
 
-    if (alertQueue.includes(payload.msgId)) {
-      // show payload info in a browser alert box.
-      alert(
-        `alert for msgId ${payload.msgId}:\n${JSON.stringify(
-          payload.data.result
-        )}`
-      );
-      alertQueue.splice(alertQueue.indexOf(payload.msgId), 1);
-    }
-
-    delete msgQueue[payload.msgId];
-  }
-
-  return payload;
+    return payload;
 }
 
 /**
@@ -40,32 +37,32 @@ function handleWebclientMessage(payload) {
  * @link https://help.optimal-systems.com/enaio_develop/display/WEB/5.2+Kommunikation
  */
 async function sendWebclientMessage(
-  payload,
-  targetOrigin = "*",
-  triggerAlert = false
+    payload,
+    targetOrigin = "*",
+    triggerAlert = false
 ) {
-  const msgId = Math.random().toString(36).substr(2, 8);
-  payload.push({ msgId });
+    const msgId = Math.random().toString(36).substr(2, 8);
+    payload.push({msgId});
 
-  if (triggerAlert) {
-    alertQueue.push(msgId);
-  }
+    if (triggerAlert) {
+        alertQueue.push(msgId);
+    }
 
-  let _resolve, _reject;
-  const promise = new Promise((resolve, reject) => {
-    _resolve = resolve;
-    _reject = reject;
-  });
+    let _resolve, _reject;
+    const promise = new Promise((resolve, reject) => {
+        _resolve = resolve;
+        _reject = reject;
+    });
 
-  msgQueue[msgId] = { resolve: _resolve, reject: _reject };
+    msgQueue[msgId] = {resolve: _resolve, reject: _reject};
 
-  // "window" is the Dashlet's JavaScript Window object. Ref: https://developer.mozilla.org/en-US/docs/Web/API/Window
-  // "parent" is the enaio® webclient Window object.
-  // postMessage" is the browser API used to communicate between enaio® webclient and the Dashlet. Ref: https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
-  window.parent.postMessage(payload, targetOrigin);
-  return promise;
+    // "window" is the Dashlet's JavaScript Window object. Ref: https://developer.mozilla.org/en-US/docs/Web/API/Window
+    // "parent" is the enaio® webclient Window object.
+    // postMessage" is the browser API used to communicate between enaio® webclient and the Dashlet. Ref: https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
+    window.parent.postMessage(payload, targetOrigin);
+    return promise;
 }
 
 // Export functions to be used in other JavaScript files.
 // Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
-export { handleWebclientMessage, sendWebclientMessage };
+export {handleWebclientMessage, sendWebclientMessage};
